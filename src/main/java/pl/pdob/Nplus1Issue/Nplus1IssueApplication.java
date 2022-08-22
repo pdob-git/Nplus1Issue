4db20eb2584eb3b7948b91b2e60e62337612c7e0
Pawel <synk@poczta.onet.pl>
2022-08-22 11:30:34 +0200

    Add project files

---
+++ src/main/java/pl/pdob/Nplus1Issue/Nplus1IssueApplication.java
@@ -0,0 +1,13 @@
+package pl.pdob.Nplus1Issue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Nplus1IssueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Nplus1IssueApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/pdob/Nplus1Issue/Nplus1IssueApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.pdob.Nplus1Issue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Nplus1IssueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
